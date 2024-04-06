049a62552ea413c4ed8043a640927fce3c4a2c92
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-04-06 00:15:53 -0700

    creamos una segunda tabla

--- input.lua
+++ input.lua
@@ -1,13 +1,14 @@
 -- input.lua
+local boardModule = require("board")
 local constants = require("constants")
 
-local M = {}
-
-function M.handleMousePressed(x, y, board, boardSize, tileSize)
+local function handleMouseClick(board, x, y)
+    local tileSize = 50
     local gridX = math.floor(x / tileSize) + 1
     local gridY = math.floor(y / tileSize) + 1
     
-    if gridX >= 1 and gridX <= boardSize and gridY >= 1 and gridY <= boardSize then
+    if gridX >= 1 and gridX <= #board and gridY >= 1 and gridY <= #board then
+        -- Cambiar el estado de la casilla dependiendo del estado actual
         if board[gridY][gridX] == constants.EMPTY then
             board[gridY][gridX] = constants.WATER_HIT
         elseif board[gridY][gridX] == constants.SHIP then
@@ -16,4 +17,6 @@ function M.handleMousePressed(x, y, board, boardSize, tileSize)
     end
 end
 
-return M
+return {
+    handleMouseClick = handleMouseClick
+}
--- main.lua
+++ main.lua
@@ -3,12 +3,15 @@ local boardModule = require("board")
 local constants = require("constants")
 local input = require("input")
 
-local board
+local board1
+local board2
 local boardSize = 9
 local tileSize = 50
+local margin = 10
 
 function love.load()
-    board = boardModule.createBoard(boardSize)
+    board1 = boardModule.createBoard(boardSize)
+    board2 = boardModule.createBoard(boardSize)
 end
 
 function love.update(dt)
@@ -16,9 +19,46 @@ function love.update(dt)
 end
 
 function love.draw()
-    boardModule.drawBoard(board, tileSize)
+    -- Dibujar el primer tablero
+    for y = 1, boardSize do
+        for x = 1, boardSize do
+            local posX = (x - 1) * tileSize
+            local posY = (y - 1) * tileSize
+            
+            if board1[y][x] == constants.EMPTY then
+                love.graphics.setColor(0, 0, 255)  -- Agua
+            elseif board1[y][x] == constants.SHIP then
+                love.graphics.setColor(100, 100, 100)  -- Barco sin golpear
+            elseif board1[y][x] == constants.SHIP_HIT then
+                love.graphics.setColor(255, 0, 0)  -- Barco golpeado
+            elseif board1[y][x] == constants.WATER_HIT then
+                love.graphics.setColor(150, 150, 255)  -- Agua golpeada
+            end
+            love.graphics.rectangle("fill", posX, posY, tileSize, tileSize)
+        end
+    end
+    
+    -- Dibujar el segundo tablero al lado derecho del primero
+    for y = 1, boardSize do
+        for x = 1, boardSize do
+            local posX = boardSize * tileSize + margin + (x - 1) * tileSize
+            local posY = (y - 1) * tileSize
+            
+            if board2[y][x] == constants.EMPTY then
+                love.graphics.setColor(255, 255, 0)  -- Otro color
+            elseif board2[y][x] == constants.SHIP then
+                love.graphics.setColor(200, 200, 0)  -- Otro color para barcos
+            elseif board2[y][x] == constants.SHIP_HIT then
+                love.graphics.setColor(255, 150, 0)  -- Otro color para barcos golpeados
+            elseif board2[y][x] == constants.WATER_HIT then
+                love.graphics.setColor(200, 200, 255)  -- Otro color para agua golpeada
+            end
+            love.graphics.rectangle("fill", posX, posY, tileSize, tileSize)
+        end
+    end
 end
 
 function love.mousepressed(x, y, button)
-    input.handleMousePressed(x, y, board, boardSize, tileSize)
+    input.handleMouseClick(board1, x, y)
+    input.handleMouseClick(board2, x, y)
 end
